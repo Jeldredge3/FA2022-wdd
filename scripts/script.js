219e105b1f7400bf9642763c4ce341a117b57d40
jeldredge3 <jordanmeldredge@gmail.com>
2022-09-20 21:39:17 -0600

    fixed issues with querySelector

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,7 @@
-/* Test */
+/* Last Updated Code */
+
+let lastModified = document.lastModified;
+document.querySelector('#update').textContent = lastModified;
+
+console.log("WDD 230 | Jordan Eldredge")
+console.log("Last Updated: " + lastModified);
