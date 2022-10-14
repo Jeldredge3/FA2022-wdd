24248a1bab2b5d1ee437086cf6cf22a3f7fb0d91
jeldredge3 <jordanmeldredge@gmail.com>
2022-10-13 20:58:06 -0600

    Week04 Update

---
+++ lesson4/scripts/todays-date.js
@@ -0,0 +1,7 @@
+/* Today's Date Code */
+
+const datefield = document.querySelector("#todays-date");
+const now = new Date();
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(now);
+
+datefield.innerHTML = `<em>${fulldate}</em>`;
